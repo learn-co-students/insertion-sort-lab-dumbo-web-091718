a4e7b37d8cdfedbc4a8db862f7c1aab6ca621b59
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-01-28 17:27:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(arr){
+    console.log(arr)
+    let min = arr[0]
+    let idx = 0 
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < min){
+            min = arr[i]
+            idx = i 
+        }
+    }
+    arr.splice(idx, 1)
+    return min
 }
 
-function insertionSort(array){
-
+function insertionSort(arr){  
+    let sortedArr = []
+    while(arr.length !== 0){
+        sortedArr.push(findMinAndRemove(arr))
+    }
+    return sortedArr
 }
